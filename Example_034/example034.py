49c53a5ccfb6503e8287a652835139da3b554018
Anastasia Selenchik <nastenaselenchik@gmail.com>
2023-07-26 19:38:53 +0300

    решена задача 34

---
+++ Example_034/example034.py
@@ -0,0 +1,17 @@
+def rhyme(words):
+    my_list = words.split()
+    result = []
+    volwes = 'уеыаоэяию'
+    for i in my_list:
+        counter = 0
+        for j in i:
+            if j in volwes:
+                counter += 1
+        result.append(counter)
+    result = set(result)
+    if len(result)==1:
+        print('Парам пам-пам')
+    else:
+        print('Пам парам')
+
+
